a8482c1a0e52024693c34242a64a353d3c671f09
Anthony Pardo <pardo.anthony@gmail.com>
2020-03-06 17:00:39 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,6 +2,10 @@ class Show < ActiveRecord::Base
   
   def self.highest_rating 
     max = Show.maximum('rating')
+  end
+  
+  def self.most_popular_show 
+    max = Show.highest_rating 
     show = Show.find_by(rating: max)
   end
   
