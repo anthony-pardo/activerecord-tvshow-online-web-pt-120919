c2210a410fdf12c580791001a99d363544e7c35d
Anthony Pardo <pardo.anthony@gmail.com>
2020-03-06 17:01:53 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -9,4 +9,13 @@ class Show < ActiveRecord::Base
     show = Show.find_by(rating: max)
   end
   
+  def self.lowest_rating 
+    min = Show.minimum('rating')
+  end
+  
+  def self.least_popular_show 
+    min = Show.lowest_rating 
+    show = Show.find_by(rating: min)
+  end
+  
 end
